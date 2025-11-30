8dc71f69d9558edf10785091990517b4a2c0f975
Pieter-Visscher <mail@pvisscher.nl>
2025-11-30 22:52:31 +0100

    add zot-registry

---
+++ apps/zot-registry.tf
@@ -0,0 +1,38 @@
+resource "kubernetes_manifest" "zot-registry-argocd" {
+  manifest = {
+    apiVersion = "argoproj.io/v1alpha1"
+    kind       = "Application"
+    metadata = {
+      namespace  = "argocd"
+      name = "zot-registry"
+    }
+    spec = {
+      project = "default"
+      source = {
+        repoURL = "git@github.com:Pieter-Visscher/kubernetes-argo.git"
+        path = "zot-registry/"
+        targetRevision = "HEAD"
+        directory = {
+          recurse = true
+        }
+      }
+      destination = {
+        namespace = "zot-registry"
+        server = "https://kubernetes.default.svc"
+      }
+      syncPolicy = {
+        managedNamespaceMetadata = {
+          labels = {
+            gateway-access = true
+          }
+        }
+        automated = {
+          selfHeal = true
+        }
+        syncOptions = [
+          "CreateNamespace=true"
+        ]
+      }
+    }
+  }
+}
